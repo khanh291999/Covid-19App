c69f0b2088cbea73778d2eaeed2bcdc0a3cc75c7
quockhanh <khanh451754@gmail.com>
2020-06-28 22:13:31 +0700

    Auto stash before rebase of "origin/master"

---
+++ app/src/main/java/com/example/covid_19/Model/MethodDetail.java
@@ -0,0 +1,16 @@
+package com.example.covid_19.Model;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.covid_19.R;
+
+public class MethodDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_method_detail);
+    }
+}
